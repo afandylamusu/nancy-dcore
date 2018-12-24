985124bc837553ea054fb3597a9a22a0d6dddbaf
afandylamusu <afandy.lamusu@live.co.uk>
2018-12-24 22:07:29 +0700

    Add project files.

---
+++ src/WebApplication/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Nancy.Bootstrappers.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication
+{
+    public class Bootstrapper : AutofacNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(ILifetimeScope existingContainer)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Update(existingContainer.ComponentRegistry);
+        }
+    }
+}
---
+++ src/WebApplication/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication.Startup))]
+
+namespace WebApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
